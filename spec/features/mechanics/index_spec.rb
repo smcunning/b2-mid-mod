fc5560c2be04a43908690b3d28a90a935bfb8c83
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-23 09:46:40 -0600

    Update view error in mechanic id section

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 describe 'mechanics index page' do
-  # before :each do
-  #   @mechanic_1 = Mechanic.create(name: "Sam Mills",
-  #                                 experience: 10)
-  #   @mechanic_2 = Mechanic.create(name: "Kara Smith",
-  #                                 experience: 11)
-  # end
+  before :each do
+    @mechanic_1 = Mechanic.create(name: "Sam Mills",
+                                  experience: 10)
+    @mechanic_2 = Mechanic.create(name: "Kara Smith",
+                                  experience: 11)
+  end
 
   it 'has header "All Mechanics" and a list of all mechanics names & experience' do
     visit "/mechanics"
@@ -14,7 +14,7 @@ describe 'mechanics index page' do
     within ".header" do
       expect(page).to have_content("All Mechanics")
     end
-
+  
     within "#mechanic-#{@mechanic_1.id}" do
       expect(page).to have_content(@mechanic_1.name)
       expect(page).to have_content(@mechanic_1.experience)
