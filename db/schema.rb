40cfe2fe0221001ce5f1b3f73fd9d397257f0b9d
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-23 10:52:30 -0600

    Migrate mechanic rides table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_160937) do
+ActiveRecord::Schema.define(version: 2020_10_23_165134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_10_23_160937) do
   create_table "amusement_parks", force: :cascade do |t|
     t.string "name"
     t.float "admission"
+  end
+
+  create_table "mechanic_rides", force: :cascade do |t|
+    t.bigint "mechanic_id"
+    t.bigint "ride_id"
+    t.index ["mechanic_id"], name: "index_mechanic_rides_on_mechanic_id"
+    t.index ["ride_id"], name: "index_mechanic_rides_on_ride_id"
   end
 
   create_table "mechanics", force: :cascade do |t|
@@ -32,5 +39,7 @@ ActiveRecord::Schema.define(version: 2020_10_23_160937) do
     t.index ["amusement_park_id"], name: "index_rides_on_amusement_park_id"
   end
 
+  add_foreign_key "mechanic_rides", "mechanics"
+  add_foreign_key "mechanic_rides", "rides"
   add_foreign_key "rides", "amusement_parks"
 end
