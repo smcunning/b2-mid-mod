d85c1c42119dc9c65f367efec6ae14b2ac8ee4bc
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-23 10:35:07 -0600

    Update spec and view to have average_thrill_rating in correct format

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -45,9 +45,9 @@ describe 'on a amusement parks show page' do
   it 'has the average thrill rating of rides' do
 
     visit "/amusement-parks/#{@park_1.id}"
-
+    save_and_open_page
     within ".thrill-rating" do
-      expect(page).to have_content(@park_1.average_thrill_rating)
+      expect(page).to have_content(@park_1.average_thrill_rating.to_f.round(1))
     end
   end
 end
