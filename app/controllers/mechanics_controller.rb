ef04cc9e1ec5b10429b576bceda7b89e45b7a763
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-23 11:28:15 -0600

    Update mechanics controller show action to add ride to mechanics rides

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -6,5 +6,10 @@ class MechanicsController < ApplicationController
 
   def show
     @mechanic = Mechanic.find(params[:id])
+    if params[:add_ride]
+      add_ride = Ride.find(params[:add_ride])
+      @mechanic.rides << add_ride
+      redirect_to "/mechanics/#{@mechanic.id}"
+    end
   end
 end
